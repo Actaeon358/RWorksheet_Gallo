064a03396295c211d308c2ae3181f9962d109b2b
Alfe Kevin Gallo <gallok358@gmail.com>
2023-09-21 04:05:52 +0000

    Update

--- RWorksheet_Gallo#1.R
+++ RWorksheet_Gallo#1.R
@@ -54,11 +54,69 @@ age <- c (34, 28, 22, 36, 27, 18, 52, 39, 42, 29,
     one_oneT <- seq(1,1000)
     one_oneT
 #a How many data points from 8.1 to 8.4?
-  #1 data points
+  #1a data points
     data_pts1 <- length(uno_syen)
     data_pts1
-  #2 data points 
+  #2a data points 
     data_pts2 <- length(twenty_sixty)
     data_pts2
+  #3a data points
+    data_pts3 <- length(twentsixt_mean)
+    data_pts3
+  #4a data points
+    data_pts4 <- length(fifty1_nine1)
+    data_pts4
+  #8.5a find only maximum data points until 10
+    nummax  <-seq (1,10)
+    nummax2 <- max(nummax)
+    nummax2
     
-#b 
+#9 
+    newuno_syen <- seq(100)
+    Filter(function(i) { all(i %% c(3,5,7) != 0) }, seq(100))
+    newuno_syen <- c(newuno_syen)
+    
+#10 Generate a sequence backwards of the integers from 1 to 100
+    reverse_unosyen <- seq(100,1)
+    reverse_unosyen
+    
+#11 List all the natural numbers below 25 that are multiples of 3 or 5
+    naturalnum <- seq(24,1)
+    naturalnum2 <- Filter(function(i) { all(i %% 3==0 | i %% 5==0)!= 0}, seq(24,1)) 
+    naturalnum2 <- c(naturalnum2)
+    naturalsum <- sum(naturalnum2)
+    naturalsum
+    
+    #11a  data points from 10 to 11
+    teneleven <- length(reverse_unosyen) + length(naturalsum) * length(naturalnum2)
+    teneleven
+#12 
+    { x <- 0+ x + 5 + }
+    
+#13 set vectorname score
+    score <- c(72, 86, 92, 63, 88, 89, 91, 92, 75,
+                75,77)
+    score
+    score2 <- score[2]
+    score3 <- score[3]
+    score2
+    score3
+#14 
+    vector1 <- c(1,2,NA,4,NA,6,7)
+    
+    #14a
+      print(vector1,na.print="-999")
+    #14b the NA changed to "999"
+      
+#15
+      name = readline(prompt="Input your name: ")
+      
+      age = readline(prompt="Input your age: ")
+      print(paste("My name is",name, "and I am",age ,"years old."))
+      print(R.version.string)
+      
+      # the output is "My name is Alfe and I am 19 years old."
+      
+      
+      
+    
